6ec2be4048ea346d9778678330b11403d4fe0b20
Jean Boudet <jean.boudet@cenlr.org>
2015-06-15 16:03:39 +0200

    Update ArianeItem and Ariane

--- src/components/Ariane.jsx
+++ src/components/Ariane.jsx
@@ -6,31 +6,6 @@ if (process.env.BROWSER) {
 }
 
 /**
- * Last item for breadcrumb
- * @author Jean BOUDET
- */
-class ArianeItemLast extends React.Component
-{
-	constructor(props) {
-		super(props);	
-	}
-
-	shouldComponentUpdate(nextProps) {
-		return nextProps.children !== this.props.children;	
-	}
-
-	render() {
-		return (
-			<NavLink routeName={this.props.route} navParams={this.props.navParams}>
-				<span className="ariane-item-last">
-					<strong>{this.props.children}</strong>
-				</span>
-			</NavLink>
-		);		
-	}
-}
-	
-/**
  * Item for breadcrumb
  * @author Jean BOUDET
  */
@@ -38,21 +13,62 @@ class ArianeItem extends React.Component
 {
 	constructor(props) {
 		super(props);	
+		this.defaultProps = {
+			withLink: false,
+			isLast: false
+		}
 	}
 
 	shouldComponentUpdate(nextProps) {
 		return nextProps.children !== this.props.children;	
 	}
 
-	render() {
+	/**
+	 * Is last item ?
+	 * If last return strong element
+	 */
+	_isLast() {
+		return (this.props.isLast) 
+			? <strong>{this.props.children}</strong>
+			: this.props.children;
+	}
+
+	/**
+	 * Item with no link (a element)
+	 */
+	_withNoLink() {
+		return (
+			<span data-cdnom={this.props.navParams.cdnom} className="ariane-item" onClick={this.context.arianeCallback}>
+				{this._isLast()}
+			</span>
+		);
+	}
+
+	/**
+	 * Item with link (a element)
+	 */
+	_withLink() {
 		return (
 			<NavLink routeName={this.props.route} navParams={this.props.navParams}>
 				<span className="ariane-item">
-					{this.props.children}
+					{this._isLast()}
 				</span>
 			</NavLink>
-		);	
+		);
 	}
+
+	render() {
+		if (this.props.withLink) {
+			return this._withLink();
+		}
+		return this._withNoLink();
+	}
+}
+
+// For execute special callBack 
+// from ExploreSubTaxon component
+ArianeItem.contextTypes = {
+	arianeCallback: React.PropTypes.func
 }
 
 /**
@@ -72,28 +88,31 @@ class Ariane extends React.Component
 	_createItems() {
 		var n            = this.props.parents.size;
 		var atlasUriName = this.context.atlasUriName;
+		var withLink     = this.props.withLink;
 		return this.props.parents.map((parent, i) => {
 			var res;
+			var params = {name: atlasUriName, cdnom: parent.get("cdnom")};
 			switch (i) {
 				case 0:
 					res = (
-						<ArianeItem key={i} route="atlas" navParams={{name: atlasUriName}}>
+						<ArianeItem key={i} route="atlas" withLink={withLink}
+							navParams={params}>
 							{parent.get("name")}
 						</ArianeItem>
 					);
 					break;
 				case n - 1:
 					res = (
-						<ArianeItemLast key={i} route="taxon" 
-							navParams={{name: atlasUriName, cdnom: parent.get("cdnom")}}>
+						<ArianeItem key={i} route="taxon" isLast={true} withLink={withLink}
+							navParams={params}>
 							{parent.get("name")}
-						</ArianeItemLast>
+						</ArianeItem>
 					);
 					break;
 				default:
 					res = (
-						<ArianeItem key={i} route="taxon" 
-							navParams={{name: atlasUriName, cdnom: parent.get("cdnom")}}>
+						<ArianeItem key={i} route="taxon" withLink={withLink} 
+							navParams={params}>
 							{parent.get("name")}
 						</ArianeItem>
 					);
