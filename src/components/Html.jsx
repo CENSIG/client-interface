ca979907825dd54987c2a3feb51e1b731df5d48c
Jean Boudet <jean.boudet@cenlr.org>
2015-06-16 15:18:59 +0200

    Update Html structure

--- src/components/Html.jsx
+++ src/components/Html.jsx
@@ -1,6 +1,11 @@
 import React						from "react";
 import ApplicationStore from "../stores/ApplicationStore";
 
+const scriptsProdName = [
+	"/static/vendors.bundle.js",
+	"/static/app.bundle.js"
+];
+
 /**
  * A component for HTML structure
  * @author Jean BOUDET
@@ -11,8 +16,35 @@ class Html extends React.Component
 		super(props);
 	}
 
-	_handleClickBody(e) {
-		console.log("body clike");	
+	/**
+	 * Get css bundle or not if dev mode
+	 * is enable
+	 */
+	_getLink() {
+		var link;
+		if (this.props.mode !== "development") {
+			link = (
+				<link rel="stylesheet" media="screen" href="/static/bundle.css" /> 	
+			);	
+		}
+		return link;
+	}
+
+	/**
+	 * Get js script bundle
+	 */
+	_getScript() {
+		var script;
+		if (this.props.mode === "development") {
+			script = (
+				<script src="/static/bundle.js"></script>
+			);
+		}	else {
+			script = scriptsProdName.map(name => {
+				return <script src={name}></script>;
+			});
+		}
+		return script;
 	}
 
 	render() {
@@ -20,12 +52,13 @@ class Html extends React.Component
 			<html lang="fr">
 				<head>
 					<meta charset="UTF-8" />
+					{this._getLink()}
 					<title>{this.props.context.getStore(ApplicationStore).getCurrentTitlePage()}</title>
 				</head>
-				<body onClick={this._handleClickBody}>
+				<body>
 					<div className="main" dangerouslySetInnerHTML={{__html: this.props.markup}}></div>
 					<script dangerouslySetInnerHTML={{__html: this.props.state}}></script>
-					<script src="/static/bundle.js"></script>
+					{this._getScript()}
 				</body>
 			</html> 	
 		);
