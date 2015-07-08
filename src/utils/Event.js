0c1348d0e0818762e10ab96923fb1edc2f098fd0
Jean Boudet <jean.boudet@cenlr.org>
2015-07-08 10:25:26 +0200

    Add Event file

---
+++ src/utils/Event.js
@@ -0,0 +1,21 @@
+import keyMirror from "keymirror";
+
+export default keyMirror({
+	PARENTS                : null,
+	FIRST_CHILDS           : null,
+	INFO                   : null,
+	GRILLE_10              : null,
+	BROTHERS               : null,
+	NOT_PARENTS            : null,
+	NOT_FIRST_CHILDS       : null,
+	NOT_INFO               : null,
+	NOT_GRILLE_10          : null,
+	NOT_BROTHERS           : null,
+	EXPLORE_SUB            : null,
+	EXPLORE_SUP            : null,
+	EXPLORE_FIRST_CHILDS   : null,
+	SEARCH_CHILDS          : null,
+	REQUEST_SEARCH_PENDING : null,
+	RESET_SEARCH_CHILDS    : null,
+	NOT_SEARCH_CHILDS      : null
+});
