5e529bed0c212a5f03498b56ff33d1370f5a700a
Jean Boudet <jean.boudet@cenlr.org>
2015-07-08 10:08:23 +0200

    Add Api dependency

--- src/configs/appConfig.js
+++ src/configs/appConfig.js
@@ -1,11 +1,15 @@
+import Api from "../utils/Api";
+
 /**
  * Configuration for application
  * @author Jean BOUDET
  */
 export default {
-	"api": {
-		"ip":"127.0.0.1"
-	},
+	"api": new Api({
+		"ip"   : (process.env.BROWSER) ? "127.0.0.1" : process.env.API_PORT_9000_TCP_ADDR,
+		"port" : 9000,
+		"name" : "apiv1"
+	}),
 
 	"atlas": {
 		"papillons": {
