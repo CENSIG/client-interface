4c1a189f6bff7087ddf4b0e8ad63f4e2b2f6caf4
Jean Boudet <jean.boudet@cenlr.org>
2015-07-29 14:45:53 +0200

    Add phenologie chart

---
+++ src/components/PhenologieChart.jsx
@@ -0,0 +1,45 @@
+import React from "react";
+import {connectToStores} from "fluxible/addons";
+
+import PhenologieStore from "../stores/PhenologieStore";
+import SeveralBarChart from "../thirdparty/react-d3/src/severalBarChart/SeveralBarChart";
+
+class PhenologieChart extends React.Component
+{
+	constructor(props) {
+		super(props);	
+	}
+
+	_getChart(data) {
+		var chart = null;
+		if (data.size !== 0) {
+			chart = <SeveralBarChart
+				legend={true}
+				nameLabelProp="mois"
+				nameValueProp={["total","adultes","larves"]}
+				data={data.toJS()}
+				margins={{top: 15, right: 20, bottom: 60, left: 60}}
+				widthWithLegend="100%"
+				width="500"
+				height={200}
+			/>;
+		}
+		return chart; 
+	}
+
+	render() {
+		return (
+			<div>
+				{this._getChart(this.props.data)}
+			</div>
+		)	
+	}
+}
+
+PhenologieChart = connectToStores(PhenologieChart, [ PhenologieStore ], (context, props) => {
+	return {
+		data: context.getStore(PhenologieStore).getState()
+	}
+});
+
+export default PhenologieChart;
