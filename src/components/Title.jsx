efa5a7bfba66b385bb55fe8ad8fe4e75ce66f690
Jean Boudet <jean.boudet@cenlr.org>
2015-08-04 17:30:08 +0200

    Add components Title and her config

---
+++ src/components/Title.jsx
@@ -0,0 +1,41 @@
+import React from "react";
+import {panelTitle} from "../configs/appConfig";
+
+class Title extends React.Component
+{
+	constructor(props)
+	{
+		super(props);
+		this.renderMap = {
+			"firstChild": this._buildFirstChildTitle,
+			"brothersNav": this._buildBrothersNavTitle
+		}
+	}
+
+	_buildFirstChildTitle(rang, name)
+	{
+		let current  = panelTitle[rang].firstChildChart;
+		let adjectif = current.adjectif;
+		let label    = current.label;
+		return (
+			<h3>Répartition des {adjectif} {label} de {name}</h3>
+		);
+	}
+
+	_buildBrothersNavTitle(rang, name)
+	{
+		let current = panelTitle[rang].brothersNav;
+		let adverbe = current.adverbe;
+		let label   = current.label;
+		return (
+			<h3>Autre taxon {adverbe} même {label} que {name}</h3>
+		);
+	}
+
+	render() {
+		var {rang, name, type} = this.props;	
+		return this.renderMap[type](rang, name);
+	}
+}
+
+export default Title;
---
+++ src/configs/panelTitle.js
@@ -0,0 +1,43 @@
+export default {
+	OR: {
+		firstChildChart: {
+			adjectif: "principales",
+			label: "familles"
+		},
+		brothersNav: {
+			adverbe: "du",
+			label: "règne"
+		}
+	},
+	FM: {
+		firstChildChart: {
+			adjectif: "principaux",
+			label: "genres"
+		},
+		brothersNav: {
+			adverbe: "du",
+			label: "ordre"
+		}
+	},
+	GN: {
+		firstChildChart: {
+			adjectif: "principales",
+			label: "espèces"
+		},
+		brothersNav: {
+			adverbe: "de la",
+			label: "famille"
+		}
+	},
+	ES: {
+		firstChildChart: {
+			adjectif: "principales",
+			label: "sous-espèces"
+		},
+		brothersNav: {
+			adverbe: "du",
+			label: "genre"
+		}
+	}
+};
+
