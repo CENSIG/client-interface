9740f9bb8d9725c4d11b28c08179d507303ad644
Jean Boudet <jean.boudet@cenlr.org>
2015-08-04 17:27:28 +0200

    Update export

--- src/configs/themesForMap.js
+++ src/configs/themesForMap.js
@@ -1,10 +1,12 @@
-export var base = {
-	grades: [0, 5, 50, 100, 300],
-	colors(n) {
-		return n < 5   ? "#fee5d9" :
-					 n < 50  ? "#fcae91" :
-					 n < 100 ? "#fb6a4a" :
-					 n < 300 ? "#de2d26" :
-										 "#a50f15";
+export default {
+	"base": {
+		grades: [0, 5, 50, 100, 300],
+		colors(n) {
+			return n < 5   ? "#fee5d9" :
+						 n < 50  ? "#fcae91" :
+						 n < 100 ? "#fb6a4a" :
+						 n < 300 ? "#de2d26" :
+											 "#a50f15";
+		}
 	}
-}
+};
