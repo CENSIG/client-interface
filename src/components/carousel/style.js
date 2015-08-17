ab64459e149781ac2a9a7d7045284503fe20e7e7
Jean Boudet <jean.boudet@cenlr.org>
2015-08-17 10:40:53 +0200

    Add style for controls

--- src/components/carousel/style.js
+++ src/components/carousel/style.js
@@ -1,11 +1,50 @@
+const borderColorArrow = "#818181";
+const borderArrow = `1px solid ${borderColorArrow}`;
+
 export default {
 	figure: {
 		display: "flex",
 		justifyContent: "center",
-		alignItems: "center"
+		alignItems: "center",
+		flexWrap: "wrap"
 	},
 
 	img: {
 		margin: "0 10px 0 0"	
+	},
+
+	arrowHidden: {
+		display: "none"
+	},
+
+	responsive: {
+		"@media (max-width: 800px)": {
+			display: "none"	
+		}
+	},
+
+	textArrow: {
+		display: "block",
+		whiteSpace: "nowrap",
+		textIndent: "100%",
+		overflow: "hidden"
+	},
+
+	arrowRight: {
+		borderTop: borderArrow,
+		borderRight: borderArrow, 
+		width: 20,
+		height:20,
+		cursor: "pointer",
+		transform: "rotate(45deg)"
+	},
+
+	arrowLeft: {
+		borderTop: borderArrow,
+		borderLeft: borderArrow, 
+		width: 20,
+		height:20,
+		cursor: "pointer",
+		transform: "rotate(-45deg)"
 	}
 }
