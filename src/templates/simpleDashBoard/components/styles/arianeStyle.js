6bc0122d13f9fc340033488a565db988a7faf889
Jean Boudet <jean.boudet@cenlr.org>
2015-08-17 15:57:09 +0200

    Add style for baseAriane

---
+++ src/templates/simpleDashBoard/components/styles/arianeStyle.js
@@ -0,0 +1,13 @@
+export default {
+	base: {
+		display: "inline-block",
+		textAlign: "right",
+		padding: "0 70px 0 0",
+		verticalAlign: "middle",
+		width: "50%",
+
+		"@media (max-width: 1200px)": {
+			display: "none"
+		}
+	}
+}
