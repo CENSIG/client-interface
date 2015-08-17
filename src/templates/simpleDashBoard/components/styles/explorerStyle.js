cc21db3a848c58372e67452c90048188006cfaa4
Jean Boudet <jean.boudet@cenlr.org>
2015-08-17 15:59:16 +0200

    Add style for explorer

---
+++ src/templates/simpleDashBoard/components/styles/explorerStyle.js
@@ -0,0 +1,25 @@
+export default {
+	baseExplorer: {
+		position: "fixed",
+		right: "10px",
+		top: "43px",
+		zIndex: 1030,
+	},
+
+	baseResponsive: {
+		"@media (max-width: 560px)": {
+			display: "none"	
+		}	
+	},
+
+	button: {
+		cursor: "pointer",
+		margin: "0 0 0 auto"
+	},
+
+	loading: {
+		height: 50,
+		margin: "30px auto 0",
+		width: "1%"
+	}
+};
