084a5ec5e3c934684f88575966732eddebaca0ce
Jean Boudet <jean.boudet@cenlr.org>
2015-08-17 16:00:10 +0200

    Add style for main title

---
+++ src/templates/simpleDashBoard/components/styles/mainTitleStyle.js
@@ -0,0 +1,15 @@
+export default {
+	color: "#aeaeae",
+	fontSize: "1.2em",
+	lineHeight: "70px",
+
+	"@media (min-width: 780px)": {
+		margin: "0 15px"	
+	},
+
+	"@media (max-width: 780px)": {
+		display: "inline-block",
+		textAlign: "right",
+		width: "90%"
+	}
+};
