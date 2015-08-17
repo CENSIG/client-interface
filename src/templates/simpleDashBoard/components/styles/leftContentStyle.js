5721f2090729610beeaf848d1c0eb669340966e4
Jean Boudet <jean.boudet@cenlr.org>
2015-08-17 15:59:45 +0200

    Add style for left content nav

---
+++ src/templates/simpleDashBoard/components/styles/leftContentStyle.js
@@ -0,0 +1,20 @@
+export default {
+	def: {
+		width: "100%"
+	},
+
+	withRight: {
+		width: "50%"
+	},
+
+	responsive: {
+		"@media (max-width: 780px)": {
+			margin: "auto",
+			width: "95%"	
+		},
+
+		"@media (min-width: 1200px)": {
+			display: "inline-block"	
+		}
+	}
+}
