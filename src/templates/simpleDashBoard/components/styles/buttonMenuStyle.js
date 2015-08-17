3defdb18a4eddc50cef30810a1d670f5f556d272
Jean Boudet <jean.boudet@cenlr.org>
2015-08-17 15:57:52 +0200

    Add style for button menu

---
+++ src/templates/simpleDashBoard/components/styles/buttonMenuStyle.js
@@ -0,0 +1,18 @@
+export default {
+	button : {
+		background: "url('/cdn/menu.svg') no-repeat",
+		display: "block",
+		textIndent: "100%",
+		overflow: "hidden",
+		whiteSpace: "nowrap"
+	},
+
+	container: {
+		display: "inline-block",
+		verticalAlign: "middle",
+		width: "5%",
+		"@media (min-width: 780px)": {
+			display: "none"
+		}
+	}
+};
