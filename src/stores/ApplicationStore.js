8df66dfb2f8060b5bdd3982eff4d2ea0b8740acb
Jean Boudet <jean.boudet@cenlr.org>
2015-08-21 08:54:18 +0200

    Update for auth

--- src/stores/ApplicationStore.js
+++ src/stores/ApplicationStore.js
@@ -1,5 +1,6 @@
 import BaseStore from "fluxible/addons/BaseStore";
 import Event from "../utils/Event";
+import Immutable from "immutable";
 
 /**
  * Store for Application
@@ -10,11 +11,16 @@ class ApplicationStore extends BaseStore
 	constructor(dispatcher) {
 		super(dispatcher);
 		this.currentTitlePage = null;
+		this.auth = Immutable.Map(); 
 	}
 	
 	_handleTitle(title) {
 		this.currentTitlePage = title;
 		this.emitChange();
+	}
+
+	_handleToken(auth) {
+		this.auth = Immutable.fromJS(auth);
 	}
 
 	getCurrentTitlePage() {
@@ -23,7 +29,8 @@ class ApplicationStore extends BaseStore
 
 	getState() {
 		return {
-			currentTitlePage : this.currentTitlePage
+			currentTitlePage : this.currentTitlePage,
+			auth: this.auth
 		};
 	}
 	
@@ -33,11 +40,13 @@ class ApplicationStore extends BaseStore
 
 	rehydrate(state) {
 		this.currentTitlePage = state.currentTitlePage;
+		this.auth = Immutable.fromJS(state.auth);
 	}
 }
 
 ApplicationStore.storeName = "ApplicationStore";
 ApplicationStore.handlers = {};
 ApplicationStore.handlers[Event.UPDATE_TITLE] = "_handleTitle";
+ApplicationStore.handlers["ACCESS_TOKEN"] = "_handleToken";
 
 export default ApplicationStore;
