60f63dacc0942da9e9d3f4c8e333f2da062c1742
Jean Boudet <jean.boudet@cenlr.org>
2015-08-27 16:17:30 +0200

    Store alphabet observateurs

--- src/stores/TaxonStore.js
+++ src/stores/TaxonStore.js
@@ -11,7 +11,6 @@ class TaxonStore extends BaseStore
 {
 	constructor(dispatcher) {
 		super(dispatcher);
-		this.current     = null;
 		this.parents = Immutable.List();
 		this.photos = Immutable.List();
 		this.grille10 = Immutable.Map();
@@ -19,6 +18,7 @@ class TaxonStore extends BaseStore
 		this.info = Immutable.Map();
 		this.phenologie = Immutable.List();
 		this.monographies = Immutable.List();
+		this.alphabetObservateurs = Immutable.List();
 	}
 
 	_handleParents(parents) {
@@ -31,13 +31,28 @@ class TaxonStore extends BaseStore
 		this.emitChange();
 	}
 
+	_handleNotPhotos() {
+		this.photos = Immutable.List();	
+		this.emitChange();
+	}
+
 	_handleGrille10(grille10) {
 		this.grille10 = grille10;	
 		this.emitChange();
 	}
 
+	_handleNotGrille10() {
+		this.grille10 = Immutable.Map();	
+		this.emitChange();
+	}
+
 	_handleFirstChilds(firstChilds) {
 		this.firstChilds = firstChilds;	
+		this.emitChange();
+	}
+
+	_handleNotFirstChilds() {
+		this.firstChilds = Immutable.List();	
 		this.emitChange();
 	}
 
@@ -56,13 +71,24 @@ class TaxonStore extends BaseStore
 		this.emitChange();
 	}
 
-	_handleCurrent(current) {
-		this.current = current;
+	_handleNotMonographies() {
+		this.monographies = Immutable.List();	
+		this.emitChange();
+	}
+
+	_handleAlphabetObservateurs(alphabet) {
+		this.alphabetObservateurs = alphabet;	
+		this.emitChange();
+	}
+
+	_handleNotAlphabetObservateurs() {
+		this.alphabetObservateurs = Immutable.List();	
 		this.emitChange();
 	}
 
 	getState() {
 		return {
+			alphabetObservateurs: this.alphabetObservateurs,
 			firstChilds: this.firstChilds,
 			grille10: this.grille10,
 			info: this.info,
@@ -78,6 +104,7 @@ class TaxonStore extends BaseStore
 	}
 
 	rehydrate(state) {
+		this.alphabetObservateurs = Immutable.fromJS(state.alphabetObservateurs);
 		this.firstChilds = Immutable.fromJS(state.firstChilds);
 		this.grille10 = Immutable.fromJS(state.grille10);
 		this.info = Immutable.fromJS(state.info);
@@ -97,5 +124,10 @@ TaxonStore.handlers[Event.FIRST_CHILDS] = "_handleFirstChilds";
 TaxonStore.handlers[Event.INFO] = "_handleInfo";
 TaxonStore.handlers[Event.PHENOLOGIE] = "_handlePhenologie";
 TaxonStore.handlers[Event.MONOGRAPHIES] = "_handleMonographies";
+TaxonStore.handlers[Event.ALPHABET_OBS] = "_handleAlphabetObservateurs";
+TaxonStore.handlers[Event.NOT_PHOTO] = "_handleNotPhotos";
+TaxonStore.handlers[Event.NOT_GRILLE_10] = "_handleNotGrille10";
+TaxonStore.handlers[Event.NOT_FIRST_CHILDS] = "_handleNotFirstChilds";
+TaxonStore.handlers[Event.NOT_MONOGRAPHIES] = "_handleNotMonographies";
 
 export default TaxonStore;
